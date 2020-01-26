636040d2935c445d7e14fa9336c326ab0a3d27d7
Mayron Alves <mayroonalves@gmail.com>
2020-01-26 00:24:19 -0300

    update to set settings

--- src/Core/Config.php
+++ src/Core/Config.php
@@ -4,7 +4,7 @@ namespace Bling\Core;
 
 class Config
 {
-    public static function configure(string $apiToken, bool $debug = false, bool $http_errors = false)
+    public static function configure(string $apiToken, bool $debug = false, bool $http_errors = false) : array
     {
         return [
             'base_uri' => 'https://bling.com.br/Api/v2/',
